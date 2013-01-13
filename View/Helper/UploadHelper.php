c3dfe59a663c124a623042b926d2582b93db7117
zoghal <saleh.souzanchi@gmail.com>
2013-01-13 18:03:31 +0330

    fix and update for cake 2.3

--- View/Helper/UploadHelper.php
+++ View/Helper/UploadHelper.php
@@ -26,7 +26,7 @@ class UploadHelper extends AppHelper {
 		return $this->Html->link($title, $this->url($data, $field, $urlOptions), $htmlOptions);
 	}
 
-	public function url($data, $field, $options = array()) {
+	public function url($data = null , $field = false, $options = array()) {
 		$options += array('style' => 'original', 'urlize' => true);
 		list($model, $field) = explode('.', $field);
 		if(is_array($data)) {
